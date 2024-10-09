c3999266e2a0778f7c9bf753e26a585f35546bad
Jeff Lee <jeff@alephx.xyz>
2024-10-08 22:21:25 -0400

    chore(config): add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://alephx-aleo.github.io',
+	base: 'aleo-sdk-node-docs',
 	integrations: [
 		starlight({
 			title: 'Aleo SDK Node',
